96a60710a6a26d63be2a744e4b8911aa71bccfb3
estelasanchez7 <stelasv7@gmail.com>
2023-05-25 12:29:09 +0200

    first commit

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Dashboard from "./routes/Dashboard"
+import Home from "./routes/Home"
+import OnBoarding from "./routes/Onboarding"
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Dashboard />,
+        
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <OnBoarding />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
