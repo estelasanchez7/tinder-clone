4c7e75f362afc74c4194a11567ac86e92ce2fcda
estelasanchez7 <stelasv7@gmail.com>
2023-06-09 09:31:24 +0200

    react-cookies

--- vite-project/server/main.jsx
+++ vite-project/server/main.jsx
@@ -1,17 +1,17 @@
 const PORT = 8000;
 const express = require("express");
 const { MongoClient } = require("mongodb");
-const {v4:uuidv4} = require('uuid')
-const bcrypt = require('bcrypt')
-const jwt = require ('jsonwebtoken')
-const cors = require ('cors')
-
+const {v4: uuidv4} = require('uuid')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
 
 const uri =
   "mongodb+srv://softengineer:mypassword@cluster0.gynkk8j.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
 const app = express();
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 // Default
 app.get("/", (req, res) => {
@@ -21,43 +21,44 @@ app.get("/", (req, res) => {
 // Sign up to the Database
 app.post("/signup", async (req, res) => {
   const client = new MongoClient(uri);
-  console.log(req.body)
-  const {email, password} = req.body
+  const { email, password } = req.body;
 
-  const generatedUserId = uuidv4()
-  const hashedPassword = await bcrypt.hash(password,10)
+  const generatedUserId = uuidv4();
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-  try{
-    client.connect()
-    const database = client.db("app-data")
+  try {
+    await client.connect();
+    const database = client.db("app-data");
     const users = database.collection("users");
 
-    const existingUser = users.findOne({email})
+    const existingUser = await users.findOne({ email });
 
-    if (existingUser){
-      return res.status(409).send('User already exists! Please login')
+    if (existingUser) {
+      return res.status(409).send("User already exists! Please login");
     }
 
-    const sanitizedEmail = email.toLowerCase()
+    const sanitizedEmail = email.toLowerCase();
 
     const data = {
       user_id: generatedUserId,
       email: sanitizedEmail,
-      hashed_password: hashedPassword
-    }
+      hashed_password: hashedPassword,
+    };
 
-    const insertedUser = users.insertOne(data)
+    const insertedUser = await users.insertOne(data);
 
-    const token = jwt.sign(insertedUser,sanitizedEmail,{
-      expiresIn: 60 * 24, /* expires in 24h */
-    })
-    
-    res.status(201).json({token, userId: generatedUserId, email: sanitizedEmail})
+    const token = jwt.sign(insertedUser, sanitizedEmail, {
+      expiresIn: 60 * 24 /* expires in 24h */,
+    });
 
-  }catch(err){
-    console.log(err)
+    res
+      .status(201)
+      .json({ token, userId: generatedUserId, email: sanitizedEmail });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.close();
   }
-
 });
 
 // Get all Users by userIds in the Database
@@ -69,7 +70,7 @@ app.get("/users", async (req, res) => {
     await client.connect();
 
     // Send a ping to confirm a successful connection
-    const database = client.db("app-data")
+    const database = client.db("app-data");
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
@@ -78,7 +79,6 @@ app.get("/users", async (req, res) => {
     const returnedUsers = await users.find().toArray();
     res.send(returnedUsers);
   } finally {
-
     // Ensures that the client will close when you finish/error
     await client.close();
   }
